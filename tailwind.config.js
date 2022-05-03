6775dca37d784549c3d08259bef1bb2bc15019ab
ugochianoka <88979050+ugochianoka@users.noreply.github.com>
2022-05-03 19:38:40 +0100

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+ 
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors:{
+        ugobeke: '#800080',
+      },
+
+      fontFamily: {
+        body: ['Nunito']
+      }
+    },
+  },
+  plugins: [],
+}
